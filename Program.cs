9c4b4fa0f5ac0100bacadae9b22883cd40b42209
Yonor <6yonor@gmail.com>
2019-06-10 12:30:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace main.srv
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 9999);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(ip);
+            socket.Listen(10);
+            Console.WriteLine("Waiting for a client...");
+            Socket client = socket.Accept();
+            IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine("Connected with {0} at port {1}", clientep.Address, clientep.Port);
+
+            string welcome = "Welcome";
+            byte[] data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(welcome);
+            client.Send(data, data.Length, SocketFlags.None);
+
+            Console.WriteLine("Disconnected from {0}", clientep.Address);
+            client.Close();
+            socket.Close();
+            Console.ReadKey();
+        }
+    }
+
+}
